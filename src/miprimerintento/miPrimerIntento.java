675c7084c81b8f2b69de2323ab5c18d57a7a23dd
introduccion02 <introduccion02@lmint06.iio2.itam.mx>
2015-02-12 11:41:11 -0600

    se acabo completamente el borrar y los otros metodos.

--- src/miprimerintento/miPrimerIntento.java
+++ src/miprimerintento/miPrimerIntento.java
@@ -8,80 +8,73 @@ package miprimerintento;
  *
  * @author introduccion02
  */
-public class miPrimerIntento<T extends Comparable<T>> implements miEstructura<T>{
-   private T[] arr;
-   private int numElem=0;
-   
-   public miPrimerIntento(){
-       this.arr =(T[]) new Comparable[1];
-   }
-   
-   public miPrimerIntento(T elem){
+public class miPrimerIntento<T extends Comparable<T>> implements miEstructura<T> {
+
+    private T[] arr;
+    private int numElem = 0;
+
+    public miPrimerIntento() {
         this.arr = (T[]) new Comparable[1];
-       
-   }
+    }
+
+    public miPrimerIntento(T elem) {
+        this.arr = (T[]) new Comparable[1];
+
+    }
 
     @Override
-    public boolean borrar(T elem) throws cheched{
-        boolean res=false;
-        int i=0;
-        
-        while(i<arr.length || !res){
-            if(arr[i].equals(elem)){
-                res=true;
-            }
+    public boolean borrar(T elem) throws cheched {
+        boolean res = false;
+        int i = 0;
+
+        while (i < numElem && !arr[i].equals(elem)) {
             i++;
         }
-        
-        if(res){
-            System.out.println("todo bien");
-        }
-        else{
-            throw new cheched("no se halla ");
-        }
-        
-        
-        
+
+         if (i < numElem) {
+             arr[i]=null;
+             res=true;
+         }
+         
         return res;
     }
 
     @Override
     public int buscar(T elem) {
-        int pos=0;
-        
-        while(pos<numElem && !elem.equals(arr[pos])){
+        int pos = 0;
+
+        while (pos < numElem && !elem.equals(arr[pos])) {
             pos++;
         }
-        if(pos==numElem){
-            pos=-1;
+        if (pos == numElem) {
+            pos = -1;
         }
-        
+
         return pos;
     }
 
     @Override
     public void insertar(T elem) {
-        //if(numElem>arr.length){
-            expandCapacity();
-        //}
-        arr[numElem]=elem;
+        if(numElem>=arr.length){
+        expandCapacity();
+        }
+        arr[numElem] = elem;
         numElem++;
     }
-    
-    private void expandCapacity(){
-        T[] B= (T[]) new Comparable[arr.length+2];
-        
-        for(int i=0; i<arr.length;i++){
-            B[i]=arr[i];
+
+    private void expandCapacity() {
+        T[] B = (T[]) new Comparable[arr.length + 2];
+
+        for (int i = 0; i < arr.length; i++) {
+            B[i] = arr[i];
         }
-        arr=B;
+        arr = B;
     }
-    
-    public void nada (int i) throws miEx{
-        if(i==0){
+
+    public void nada(int i) throws miEx {
+        if (i == 0) {
             System.out.println("todo bien");
-        }
-        else{
+        } else {
             throw new miEx("prueba");
         }
     }
